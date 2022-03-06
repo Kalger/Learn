da5d006070914a573ad025550c6fa4e7ef279516
Alger <gino63o7@gmail.com>
2022-03-06 16:03:50 +0800

    Book - FirstLineCode - Activity - 3.4.4~3.4.5

--- Android/firstLineCode/3thChapter/ActivityLifeCycleTest/app/src/main/java/com/example/activitylifecycletest/MainActivity.kt
+++ Android/firstLineCode/3thChapter/ActivityLifeCycleTest/app/src/main/java/com/example/activitylifecycletest/MainActivity.kt
@@ -1,11 +1,75 @@
 package com.example.activitylifecycletest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.activitylifecycletest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState != null) {
+            val tmp = savedInstanceState.getString("data_key")
+            Log.d(TAG, "temp data is $tmp")
+        }
+
+        binding.startNormalActivity.setOnClickListener {
+            startActivity(Intent(this, NormalActivity::class.java))
+        }
+
+        binding.startDialogActivity.setOnClickListener {
+            startActivity(Intent(this, DialogActivity::class.java))
+        }
+
+        binding.finish.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val tempData = "your text"
+        outState.putString("data_key", tempData)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
     }
 }
